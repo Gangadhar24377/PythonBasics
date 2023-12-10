26a2b117ef23b8ce2b0445994ccf97eb806e2225
Gangadhar24377 <gangadharkambhamettu@gmail.com>
2023-12-10 09:27:51 +0530

    adding BandName generator

---
+++ BandNameGenerator/band_name.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + pet)    # Concatenation
+
+# Output: Your band name could be Los Angeles Dog   (if city = Los Angeles and pet = Dog)
